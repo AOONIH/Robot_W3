8f87929442055637b59d16d4fda779232cada748
gubenheft <dedamstar@gmail.com>
2019-09-23 16:27:09 +0100

    added function for centre

--- ball_tracking.py
+++ ball_tracking.py
@@ -8,7 +8,33 @@ from picamera import PiCamera
 import time
 import numpy as np
 
+def track_centre(col_mask):
+    cnts = cv2.findContours(col_mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
 
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        centre = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            #cv2.circle(frame, (int(x), int(y)), int(radius),
+            #           (0, 255, 255), 2)
+            return centre
+        else:
+            return None
+    
+    
 camera = PiCamera() # PiCamera object
 camera.resolution = (640, 480)
 camera.framerate = 30
@@ -40,7 +66,9 @@ time.sleep(2.0)
 running = True
 while running:
     # capture frames from the camera
-    for frame in camera.capture_continuous(buffer, format="bgr", use_video_port=True):
+    for stream in camera.capture_continuous(buffer, format="bgr", use_video_port=True):
+        
+        frame = stream.array
         # store the array attribute of the frame object
         frame = imutils.resize(frame, width=600)
         blurred = cv2.GaussianBlur(frame, (11, 11), 0)
@@ -76,30 +104,30 @@ while running:
             if radius > 10:
                 # draw the circle and centroid on the frame,
                 # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                           (0, 255, 255), 2)
+                #cv2.circle(frame, (int(x), int(y)), int(radius),
+                #           (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
         # update the points queue
         pts.appendleft(center)
 
         # loop over the set of tracked points
-        for i in range(1, len(pts)):
+        #for i in range(1, len(pts)):
             # if either of the tracked points are None, ignore
             # them
-            if pts[i - 1] is None or pts[i] is None:
-                continue
+            #if pts[i - 1] is None or pts[i] is None:
+                #continue
 
             # otherwise, compute the thickness of the line and
             # draw the connecting lines
-            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+            #thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            #cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
         # show the frame to our screen
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
         # clear the stream in preparation for the next frame
-        frame.truncate(0)
+        stream.truncate(0)
 
         # if the 'q' key is pressed, stop the loop
         if key == ord("q"):
