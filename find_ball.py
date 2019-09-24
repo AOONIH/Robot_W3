5ebf63ca24801ca257ca6b84e352c41a7f0862f1
liyuan xu <>
2019-09-24 12:01:40 +0100

    code refactoring. add multiple color tracking

---
+++ find_ball.py
@@ -0,0 +1,37 @@
+import cv2
+import imutils
+
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+redLower = (170, 86, 6)
+redUpper = (180, 255, 255)
+blueLower = (110, 86, 6)
+blueUpper = (130, 255, 255)
+
+def find_red(hsv):
+    return find_ball(hsv, redLower, redUpper)
+
+def find_blue(hsv):
+    return find_ball(hsv, blueLower, blueUpper)
+
+
+def find_green(hsv):
+    return find_ball(hsv, greenLower, greenUpper)
+
+
+def find_ball(hsv, lower_color, upper_color):
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        centre = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 10:
+            return centre
+        else:
+            return None
---
+++ main.py
@@ -0,0 +1,51 @@
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import numpy as np
+
+from find_ball import find_red, find_blue, find_green
+
+# construct the argument parse and parse the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--novideo", action="store_true")
+args = parser.parse_args()
+
+camera = PiCamera()  # PiCamera object
+camera.resolution = (640, 480)
+camera.framerate = 30
+window_size = 600
+buffer = PiRGBArray(camera)  # PiRGBArray object
+time.sleep(0.1)  # 100ms
+
+running = True
+while running:
+    for stream in camera.capture_continuous(buffer, format="bgr", use_video_port=True):
+
+        frame = stream.array
+        frame = imutils.resize(frame, width=window_size)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        red_center = find_red(hsv)
+        blue_center = find_blue(hsv)
+        green_center = find_green(hsv)
+
+        if not args.novideo:
+            if red_center:
+                cv2.circle(frame, red_center, 5, (0, 0, 255), -1)
+            if blue_center:
+                cv2.circle(frame, blue_center, 5, (255, 0, 0), -1)
+            if green_center:
+                cv2.circle(frame, green_center, 5, (0, 255, 0), -1)
+            cv2.imshow("Frame", frame)
+        
+        stream.truncate(0)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    running = False
+cv2.destroyAllWindows()
