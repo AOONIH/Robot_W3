0814c2fdf2f51f418d4143ec1efc6e34c0570580
liyuan xu <>
2019-09-25 17:40:38 +0100

    try another limits

--- control.py
+++ control.py
@@ -13,7 +13,7 @@ class Controller:
         self.window_width = window_width
         self.captures = False
         self.green_pop_count = 0
-        self.pop_until = None
+        self.pop_until = 0
         self.recent_pop = 0
         self.key = "green"
 
@@ -23,27 +23,31 @@ class Controller:
             self.pop_detection(con, center_info["green"][1])
 
     def pop_detection(self, con, radius):
+
         if radius is None:
             radius = 0.0
 
-        if (self.pop_until is not None and self.pop_until > time.time()):
-            self.captures = False
-
         if (not self.captures) and radius > self.window_width*0.4:
             self.captures = True
-            self.pop_until = time.time() + 3
+            self.pop_until = 10
             logger.critical("balloon captured")
+        
+        if (self.pop_until > 0):
+            self.pop_until -= 1
+            if self.pop_until == 0:
+                self.captures = False
 
         if self.captures and radius < self.window_width*0.1:
             # poped
             self.captures = False
             self.green_pop_count += 1
-            self.recent_pop = time.time()
+            self.recent_pop = 30
             logger.critical("balloon poped")
 
     def control_motor(self, con, center):
         if center is None:
-            if(self.recent_pop + 10 < time.time()):
+            if(self.recent_pop  > 0):
+                self.recent_pop -= 1
                 con.write(b"S")
             else:
                 con.write(b"R")
